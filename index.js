06fa49ed1ba0e03624245ab2a62a503b3d274dbf
Muhammad Usman <manijnb0333@gmail.com>
2022-07-11 19:45:27 +0500

    eduback

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-
 const express = require("express");
-var bodyParser = require('body-parser')
+var bodyParser = require("body-parser");
 var cors = require("cors");
 
 const connectToMongo = require("./db");
 
-
 connectToMongo();
-
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -16,7 +13,7 @@ app.use(cors());
 //app.use(express.json());
 app.use(bodyParser.json());
 
-app.use(express.json({limit: '50mb'})); 
+app.use(express.json({ limit: "50mb" }));
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -24,15 +21,10 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/profile", require("./routes/profile"));
 
-
-
-
-
-
 app.get("/", (req, res) => {
   res.send("connected to database horry yayy");
 });
 
 app.listen(PORT, () => {
-  console.log("Example app listening at",+PORT);
+  console.log("Example app listening at", +PORT);
 });
